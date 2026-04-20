32521993fe5aff0660c17e6e211bedc884500eec
thomasmackey123 <thomasmackey99@gmail.com>
2026-04-19 20:55:17 -0400

    Clean up comments in __init__.py
    Removed unnecessary comments and metadata from the file.

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,3 @@
-# Yuhang Pan (PM), Matthew Ciu, Michelle Chen, Thomas Mackey
-# Hero Wars by Teem_sqrt4469
-# SoftDev
-# P01: ArRESTed Development
-# Dec 2025
-
 from flask import Flask
 from flask import render_template
 from flask import request
